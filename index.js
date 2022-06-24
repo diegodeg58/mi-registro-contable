47fb9e4a42fa737e606802f1db01c4ce329cd050
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2022-06-24 01:05:25 -0400

    Habilita handlebars

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+process.env.CYCLIC_DB = 'important-blue-wrapCyclicDB';
+
 const express = require('express');
 const app = express();
 const hbs = require('express-handlebars');
 const jwt = require('jsonwebtoken');
+const db = require('cyclic-dynamodb');
 
 const privateKey = 'rAnDoMPaSsWoRd';
+
+app.use('/', express.static(`${__dirname}/assets/css`));
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Servidor activo en puerto ${port}`));
 
+app.set("view engine", "handlebars");
+app.engine(
+    "handlebars",
+    hbs.engine({
+        layoutsDir: `${__dirname}/views`,
+        partialsDir: `${__dirname}/views/partials`
+    })
+);
+
 app.get('/', (req, res) => {
-    res.send("Hola mundo");
-})
+    res.render("index", {
+        layout: "index"
+    });
+});
+
+app.get('/cosas', async (req, res) => {
+    let cosas = db.collection('cosas');
+    res.send(await cosas.list());
+});
+
+app.post('/cosas', async (req, res) => {
+    let cosas = db.collection('cosas');
+    let objeto1 = await cosas.set('objeto1', {
+        tipo: 'cosa'
+    });
+    res.send(objeto1);
+});

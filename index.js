feb495aee6578e30197d8f70881391d1dab7aa0f
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2022-06-23 23:27:18 -0400

    Preparando hola mundo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Testing");
+const express = require('express');
+const app = express();
+const hbs = require('express-handlebars');
+const jwt = require('jsonwebtoken');
+
+const privateKey = 'rAnDoMPaSsWoRd';
+
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`Servidor activo en puerto ${port}`));
+
+app.get('/', (req, res) => {
+    res.send("Hola mundo");
+})

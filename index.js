cb035100b5eca1d21727567d52f9e92e95ff8743
diegodeg58 <diego_deg_58@hotmail.com>
2022-07-09 20:19:02 -0400

    Refactoring y cambios menores

--- index.js
+++ index.js
@@ -37,17 +37,25 @@ app.get('/', async (req, res) => {
     })
     .catch((error) => {
         console.error("Error en la petición:", error.code);
-    })
+    });
+
     res.render("index", {
         layout: "index",
         lista: lista
     });
 });
 
-app.get('/ultimas', (req, res) => {
-    let finanzas =  db.collection("finanzas");
-    finanzas.list().then((data) => {
-        Promise.all(data.results.map(async (finanza) => {
+app.get('/movimientos', (req, res) => {
+    
+});
+
+app.get('/ultimas', async (req, res) => {
+    const tabla = 'finanzas';
+
+    let finanzas = db.collection(tabla);
+    try {
+        let lista = await finanzas.list(8);
+        lista = lista.results.map(async (finanza) => {
             const item = await finanzas.get(finanza.key);
             return {
                 id: finanza.key,
@@ -56,31 +64,93 @@ app.get('/ultimas', (req, res) => {
                 persona: item.props.persona,
                 descripcion: item.props.descripcion
             }
-        })).then((data) => {
-            res.json({
-                results: data.slice(0,8)
-            })
         })
-    })
+        let jsonData = await Promise.all(lista);
+        res.json({
+            results: jsonData
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({
+            error: "Ha ocurrido un error obteniendo los datos",
+            code: error.$metadata.httpStatusCode
+        })
+    }
 });
 
 app.get('/finanzas', async (req, res) => {
+    const tabla = 'finanzas';
+    const cantidad = parseInt(req.query.n) > 0 ? parseInt(req.query.n) : 5;
+    const pagina = parseInt(req.query.page) > 0 ? parseInt(req.query.page) : 1;
     
+    let finanzas = db.collection(tabla);
+
+    ////////////////
+    //TODO: Sección donde se obtiene un arreglo de los <id> que retorna el método list()
+    //El tamaño del arreglo depende de la cantidad de páginas que se obtenga a través de list()
+    //Para obtener el arreglo se debe realizar un ciclo while hasta que la variable LastEvaluatedKey retorne null
+    ////////////////
+
+    try {
+        let lista = await finanzas.list(cantidad, /* {
+            "keys_gsi": "finanzas",
+            "sk": "finanzas#c84e410f",
+            "pk": "finanzas#c84e410f",
+            "keys_gsi_sk": "2022-07-05T02:47:41.383Z"
+        } */);
+        const next = lista.next;
+        lista = lista.results.map(async (finanza, index) => {
+            let lastItem;
+            if(index == cantidad - 1){
+                lastItem = finanza;
+                console.log(lastItem);
+            }
+    
+            const item = await finanzas.get(finanza.key);
+            return {
+                id: finanza.key,
+                tipo: item.props.tipo,
+                monto: item.props.monto,
+                persona: item.props.persona,
+                descripcion: item.props.descripcion
+            }
+        })
+        let jsonData = await Promise.all(lista);
+        
+        res.json({
+            results: jsonData,
+            next: next
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({
+            error: "Ha ocurrido un error",
+            code: error.$metadata.httpStatusCode
+        })
+    }
 });
 
 app.get('/finanza', async (req, res) => {
     const id = req.query.id;
     const finanzas =  db.collection("finanzas");
-    const finanza = await finanzas.get(id);
-    res.json({
-        id: finanza.key,
-        tipo: finanza.props.tipo,
-        monto: finanza.props.monto,
-        fecha: finanza.props.fecha,
-        persona: finanza.props.persona,
-        descripcion: finanza.props.descripcion,
-        fijo: finanza.props.fijo
-    });
+    try {
+        const finanza = await finanzas.get(id);
+        res.json({
+            id: finanza.key,
+            tipo: finanza.props.tipo,
+            monto: finanza.props.monto,
+            fecha: finanza.props.fecha,
+            persona: finanza.props.persona,
+            descripcion: finanza.props.descripcion,
+            fijo: finanza.props.fijo
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({
+            error: "Ha ocurrido un error",
+            code: error.$metadata.httpStatusCode
+        })
+    }
 });
 
 app.post('/finanza', async (req, res) => {

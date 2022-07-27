9e4b886209ae6202ffadd41b7a572a4751697bea
diegodeg58 <diego_deg_58@hotmail.com>
2022-07-27 16:03:00 -0400

    Seguir con cambios...

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-process.env.CYCLIC_DB = 'important-blue-wrapCyclicDB';
+const db = require('./cyclicdb.js');
 
 const express = require('express');
 const app = express();
 const hbs = require('express-handlebars');
-const db = require('cyclic-dynamodb');
 const axios = require('axios').default;
 const moment = require('moment');
 const {v4} = require('uuid');
@@ -85,32 +84,9 @@ app.get('/movimientos', (req, res) => {
 });
 
 app.get('/ultimas', async (req, res) => {
-    const tabla = 'finanzas';
-
-    let finanzas = db.collection(tabla);
-    try {
-        let lista = await finanzas.list(8);
-        lista = lista.results.map(async (finanza) => {
-            const item = await finanzas.get(finanza.key);
-            return {
-                id: finanza.key,
-                tipo: item.props.tipo,
-                monto: item.props.monto,
-                persona: item.props.persona,
-                descripcion: item.props.descripcion
-            }
-        })
-        let jsonData = await Promise.all(lista);
-        res.json({
-            results: jsonData
-        });
-    } catch (error) {
-        console.error(error.message);
-        res.status(400).json({
-            error: "Ha ocurrido un error obteniendo los datos",
-            code: error.$metadata.httpStatusCode
-        })
-    }
+    const finanzas = await db.ObtenerUltimas('finanzas');
+    const status_code = finanzas ? 200 : 404;
+    res.status(status_code).json(finanzas);
 });
 
 app.get('/finanzas', async (req, res) => {
@@ -118,21 +94,21 @@ app.get('/finanzas', async (req, res) => {
     const cantidad = parseInt(req.query.n) > 0 ? parseInt(req.query.n) : 5;
     const pagina = parseInt(req.query.page) > 0 ? parseInt(req.query.page) : 1;
     
-    let finanzas = db.collection(tabla);
+    //let finanzas = db.collection(tabla);
 
     ////////////////
     //TODO: Sección donde se obtiene un arreglo de los <id> que retorna el método list()
     //El tamaño del arreglo depende de la cantidad de páginas que se obtenga a través de list()
     //Para obtener el arreglo se debe realizar un ciclo while hasta que la variable LastEvaluatedKey retorne null
     ////////////////
-
+    /*
     try {
-        let lista = await finanzas.list(cantidad, /* {
+        let lista = await finanzas.list(cantidad, {
             "keys_gsi": "finanzas",
             "sk": "finanzas#c84e410f",
             "pk": "finanzas#c84e410f",
             "keys_gsi_sk": "2022-07-05T02:47:41.383Z"
-        } */);
+        } );
         const next = lista.next;
         lista = lista.results.map(async (finanza, index) => {
             let lastItem;
@@ -163,10 +139,12 @@ app.get('/finanzas', async (req, res) => {
             code: error.$metadata.httpStatusCode
         })
     }
+    */
 });
 
 app.get('/finanza', async (req, res) => {
     const id = req.query.id;
+    /*
     const finanzas =  db.collection("finanzas");
     try {
         const finanza = await finanzas.get(id);
@@ -186,15 +164,16 @@ app.get('/finanza', async (req, res) => {
             code: error.$metadata.httpStatusCode
         })
     }
+    */
 });
 
 app.post('/finanza', async (req, res) => {
     let datos = req.body;
-    
+    /*
     const id = v4().slice(0,8);
     let finanzas = db.collection('finanzas');
     let finanza = await finanzas.set(id, datos);
-    
+    */
     res.json(datos);
 });
 

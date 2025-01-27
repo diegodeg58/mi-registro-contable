756980f9df8b6b736f6adfa99e7d0e0251c45ba6
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2025-01-27 17:02:06 -0300

    feat: Update project name and integrate PostgreSQL for user management

--- src/model/model.js
+++ src/model/model.js
@@ -1,20 +1,56 @@
-//DynamoDB
-const CyclicDB = require("@diegodeg58/cyclic.sh-dynamodb");
-const db = CyclicDB(process.env.CYCLIC_DB);
+const pg = require("pg");
+const { Pool } = pg;
 
-const tabla = 'finanzas';
-const finanzas = db.collection(tabla);
+const pool = new Pool({
+  host: "localhost",
+  user: "postgres",
+  password: "postgres",
+  database: "registro-contable",
+  port: 5432,
+});
 
 const getLastTransactions = async (qty) => {
-  return await finanzas.list(qty);
 }
 
 const getTransaction = async (id) => {
-  return await finanzas.get(id);
 }
 
 const setTransaction = async (id, datos) => {
-  return await finanzas.set(id, datos);
 }
 
-module.exports = { getLastTransactions, getTransaction, setTransaction }
+const setNewUser = async (datos) => {
+  const query = {
+    text: 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
+    values: [datos.user, datos.email, datos.password],
+  };
+  const result = await pool.query(query);
+}
+
+const getUser = async (user) => {
+  const query = {
+    text: 'SELECT * FROM users WHERE email = $1 OR username = $1',
+    values: [user],
+  };
+  const result = await pool.query(query);
+  return result.rows[0];
+}
+
+const getErrorMessage = (code) => {
+  switch (Number(code)) {
+    case 23502:
+      return "Faltan datos";
+    case 23505:
+      return "El email ya está registrado";
+    default:
+      return "Ha ocurrido un error";
+  }
+}
+
+module.exports = {
+  getLastTransactions,
+  getTransaction,
+  setTransaction,
+  setNewUser,
+  getErrorMessage,
+  getUser,
+};

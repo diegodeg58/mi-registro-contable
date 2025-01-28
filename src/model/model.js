e13371ea177f5019a6279ce6c8c82be33c6ad7ea
Diego Rojas Torrealba <diego_deg_58@hotmail.com>
2025-01-28 01:23:01 -0300

    fix: database environment variable to DB_DATABASE

--- src/model/model.js
+++ src/model/model.js
@@ -6,7 +6,7 @@ devDBConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
 };
 
